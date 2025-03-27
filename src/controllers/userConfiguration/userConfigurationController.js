6e72d3c165e309c8a2916e47ab608c1f151307b7
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-03-27 11:36:27 +0800

    fix: fix bug in adding smtp credentials
    fix bug in adding stmp credentials by correcting the arguments

--- src/controllers/userConfiguration/userConfigurationController.js
+++ src/controllers/userConfiguration/userConfigurationController.js
@@ -2,10 +2,9 @@ const smtpModel = require("../../models/userConfiguration/smtpModel");
 
 exports.addSmtpCredentials = (req, res) => {
     try {
-        const data = req.body; 
-        console.log(data);
+        const {user_id, app_pass} = req.body; 
         
-        smtpModel.addSmtpCredentials()
+        smtpModel.addSmtpCredentials(user_id, app_pass)
         res.status(200).json({ message: 'smtp credentials added' })
     } catch (error) {
         res.status(500).json({ message: error.message })
--- src/models/userConfiguration/smtpModel.js
+++ src/models/userConfiguration/smtpModel.js
@@ -1,18 +1,18 @@
+const { json } = require("express");
 const pool = require("../../config/db"); 
 
-const addSmtpCredentials = async (data) =>  {
+const addSmtpCredentials = async (user_id, app_pass) =>  {
     try {
-
         const sql = `
         INSERT INTO ats_smtp_credentials (user_id, app_password)
         VALUES (?, ?);
         `;
-        const values = [data.user_id, data.app_password];
+        const values = [user_id, app_pass];
         await pool.execute(sql, values);
-        return true
+        return 
     } catch (error) {
         console.log(error.message);
-        return false
+        return
     }
 }
 
