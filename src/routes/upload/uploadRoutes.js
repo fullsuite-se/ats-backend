6202fe921634f5bc3feffeceb2a98c0356050c76
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-02 11:27:08 +0800

    fix: use memoryStorage instead of memoryDisk since we're using buffer.

--- src/routes/upload/uploadRoutes.js
+++ src/routes/upload/uploadRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require('multer');
 const uploadController = require('../../controllers/upload/uploadController');
 
-const storage = multer.diskStorage({});
+const storage = multer.memoryStorage({});
 const upload = multer({storage});
 
 
