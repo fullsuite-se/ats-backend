a511a83b120a7d99746f26a9b44443c3af03e4f3
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-02 11:31:54 +0800

    fix: separate the config for diskstorage and memorystorage

--- src/routes/upload/uploadRoutes.js
+++ src/routes/upload/uploadRoutes.js
@@ -3,14 +3,17 @@ const router = express.Router();
 const multer = require('multer');
 const uploadController = require('../../controllers/upload/uploadController');
 
-const storage = multer.memoryStorage({});
+const storage = multer.diskStorage({});
 const upload = multer({storage});
 
+// for google drive uploads that need buffer
+const multerMemStorage = multer.memoryStorage();
+const uploadMem = multer({ storage: multerMemStorage });
 
 // /upload/cv
 router.post('/cv', upload.single('file') ,uploadController.uploadCV);
 
 // /upload/gdrive/cv
-router.post('/gdrive/cv', upload.single("file"), uploadController.uploadCVGdrive)
+router.post('/gdrive/cv', uploadMem.single("file"), uploadController.uploadCVGdrive)
 
 module.exports = router;
