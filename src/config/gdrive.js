76b63a65c01ef2b02e5d6d13332c66a82c8f8ae6
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-02 10:21:18 +0800

    refactor: separate the controller of uploading cv in gdrive from its configuration and function.

---
+++ src/config/gdrive.js
@@ -0,0 +1,70 @@
+const fs = require("fs");
+const path = require("path");
+const { google } = require("googleapis");
+const stream = require("stream");
+require("dotenv").config();
+
+const credentials = {
+    "type": process.env.TYPE,
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+    "universe_domain": process.env.UNIVERSE_DOMAIN,
+}; 
+
+const auth = new google.auth.GoogleAuth({
+  credentials: credentials,  // Use credentials object instead of keyFile
+  scopes: ["https://www.googleapis.com/auth/drive.file"],
+});
+
+const driveService = google.drive({ version: "v3", auth });
+
+// Upload File to Google Drive
+async function uploadFileToDrive(fileBuffer, fileName) {
+  const fileMetadata = {
+    name: fileName,
+    parents: [process.env.GDRIVE_FOLDER_ID], 
+  };
+
+  // Convert the file buffer to a readable stream
+  const bufferStream = new stream.PassThrough();
+  bufferStream.end(fileBuffer);
+
+  const media = {
+    mimeType: "application/octet-stream",
+    body: bufferStream,  
+  };
+
+  const response = await driveService.files.create({
+    resource: fileMetadata,
+    media: media,
+    fields: "id",
+  });
+
+  const fileId = response.data.id;
+
+  // Make the file publicly accessible
+  await driveService.permissions.create({
+    fileId,
+    requestBody: {
+      role: "reader",
+      type: "anyone",
+    },
+  });
+
+  // Get the shareable link (webViewLink) instead of the direct download URL
+  const fileData = await driveService.files.get({
+    fileId: fileId,
+    fields: "webViewLink",
+  });
+
+  return { fileId, fileUrl: fileData.data.webViewLink }; 
+}
+
+module.exports = {uploadFileToDrive}
--- src/controllers/upload/uploadController.js
+++ src/controllers/upload/uploadController.js
@@ -2,10 +2,8 @@ const pool = require('../../config/db');
 const cloudinary = require('cloudinary').v2;
 const fs = require("fs");
 const path = require("path");
-const { google } = require("googleapis");
-const stream = require("stream");
 require("dotenv").config();
-
+const gdrive = require("../../config/gdrive"); 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
@@ -36,69 +34,6 @@ exports.uploadCV = async (req, res) => {
     }
 }
 
-// Load Google Service Account Key
-const credentials = {
-    "type": process.env.TYPE,
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY,
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.UNIVERSE_DOMAIN,
-}; 
-const auth = new google.auth.GoogleAuth({
-  credentials: credentials,  // Use credentials object instead of keyFile
-  scopes: ["https://www.googleapis.com/auth/drive.file"],
-});
-
-const driveService = google.drive({ version: "v3", auth });
-
-// Upload File to Google Drive
-async function uploadFileToDrive(fileBuffer, fileName) {
-  const fileMetadata = {
-    name: fileName,
-    parents: ["1Z_F23GPD8aDcM37JElP2xAUKiad0MJ3V"], // correct - array of parent folder IDs
-  };
-
-  // Convert the file buffer to a readable stream
-  const bufferStream = new stream.PassThrough();
-  bufferStream.end(fileBuffer);
-
-  const media = {
-    mimeType: "application/octet-stream",
-    body: bufferStream,  // Now passing the stream instead of the buffer
-  };
-
-  const response = await driveService.files.create({
-    resource: fileMetadata,
-    media: media,
-    fields: "id",
-  });
-
-  const fileId = response.data.id;
-
-  // Make the file publicly accessible
-  await driveService.permissions.create({
-    fileId,
-    requestBody: {
-      role: "reader",
-      type: "anyone",
-    },
-  });
-
-  // Get the shareable link (webViewLink) instead of the direct download URL
-  const fileData = await driveService.files.get({
-    fileId: fileId,
-    fields: "webViewLink",
-  });
-
-  return { fileId, fileUrl: fileData.data.webViewLink }; // Return the sharable link
-}
-
 
 exports.uploadCVGdrive = async (req, res) => {
     try {
@@ -107,7 +42,7 @@ exports.uploadCVGdrive = async (req, res) => {
         }
 
         const { buffer, originalname } = req.file;
-        const { fileId, fileUrl } = await uploadFileToDrive(buffer, originalname);
+        const { fileId, fileUrl } = await gdrive.uploadFileToDrive(buffer, originalname);
     
         res.json({ success: true, fileId, fileUrl }); // Return the file ID and sharable link
     } catch (error) {
