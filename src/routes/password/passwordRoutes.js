1a9968045a5d9006051f1cce67f5cdc7d2cf884c
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-14 18:21:53 +0800

    add reset password route to password routes

--- src/routes/password/passwordRoutes.js
+++ src/routes/password/passwordRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const passwordController = require("../../controllers/password/passwordController"); 
 
 router.post("/resset-request", passwordController.requestReset );
-router.post("/verify-otp", passwordController.verifyOTP)
+router.post("/verify-otp", passwordController.verifyOTP); 
+router.post("/reset-password", passwordController.resetPassword);
 module.exports = router;
