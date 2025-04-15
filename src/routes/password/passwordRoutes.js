c1f6e5176890b91ba7eb8e6884cd0663e3542afa
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-15 09:06:57 +0800

    fix: correct typo in reset password route from 'resset-request' to 'reset-request'

--- src/routes/password/passwordRoutes.js
+++ src/routes/password/passwordRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const passwordController = require("../../controllers/password/passwordController"); 
 
-router.post("/resset-request", passwordController.requestReset );
+router.post("/reset-request", passwordController.requestReset );
 router.post("/verify-otp", passwordController.verifyOTP); 
 router.post("/reset-password", passwordController.resetPassword);
 module.exports = router;
