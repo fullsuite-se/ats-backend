4e808d181491d13bdd2d84a4e947599ee90681b6
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-18 16:28:57 +0800

    feat: integrate Slack messaging for new interview notifications

--- src/services/slack.js
+++ src/services/slack.js
@@ -1,19 +1,13 @@
-import Slack from '@slack/bolt';
-import dotenv from 'dotenv';
-dotenv.config();
-const userModel = require("../models/user/userModel");
+const { App } = require("@slack/bolt");
+require("dotenv").config();
 
-const app = new Slack.App({
+const app = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
     token: process.env.SLACK_BOT_TOKEN,
 })
 
-
-
 const messageBot = async (message, user_id) => {
-    const user = await userModel.getUserInfo(user_id);
-
-    const text = `${user.first_name} ${user.last_name}: ${message}`
+    const text = `${message}`
 
     await app.client.chat.postMessage({
         token: process.env.SLACK_BOT_TOKEN,
@@ -21,7 +15,6 @@ const messageBot = async (message, user_id) => {
         text: text,
         // blocks: block,
     });
-}
+};
 
-module.exports = sendMessageToSlack;
-
+module.exports = messageBot; 
