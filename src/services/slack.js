2dcd27057258798530fea848d2013ab8f862e71e
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-21 09:36:52 +0800

    feat: update applicant retrieval in Slack messaging for interviews and notes

--- src/services/slack.js
+++ src/services/slack.js
@@ -21,7 +21,8 @@ module.exports.messageBot = async (message, interviewer_id) => {
 
 module.exports.messageBotInterview = async (interviewer_id, applicant_id) => {
     const user = await userModel.getUserInfo(interviewer_id);
-    const applicant = await applicantModel.getApplicant(applicant_id);
+    let applicant = await applicantModel.getApplicant(applicant_id);
+    applicant = applicant[0];
     const message = `New interview was created for ${applicant.first_name} ${applicant.last_name}`;
 
     const text = `${user.first_name} ${user.last_name}: ${message}`;
@@ -36,9 +37,9 @@ module.exports.messageBotInterview = async (interviewer_id, applicant_id) => {
 
 module.exports.messageBotNote = async (note, interviewer_id, applicant_id) => {
     const user = await userModel.getUserInfo(interviewer_id);
-    const applicant = await applicantModel.getApplicant(applicant_id);
-    const message = `New note was added for ${applicant.first_name} ${applicant.last_name}.` + note;
-
+    let applicant = await applicantModel.getApplicant(applicant_id);
+    applicant = applicant[0];
+    const message = `New note was added for ${applicant.first_name} ${applicant.last_name}: ` + note;
     const text = `${user.first_name} ${user.last_name}: ${message}`;
 
     await app.client.chat.postMessage({
