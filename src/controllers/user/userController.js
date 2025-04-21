f3febc7ff9cd539c89d0083b74a62162dcd4866c
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-21 17:56:48 +0800

    feat: add user account creation endpoint and implement user data insertion

--- src/controllers/user/userController.js
+++ src/controllers/user/userController.js
@@ -1,7 +1,10 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const pool = require("../../config/db");
+const { v4: uuidv4 } = require("uuid");
 
 const getUserInfoSQL = `
-    SELECT 
+    SELECT
         hris_user_accounts.user_id, 
         hris_user_accounts.user_email, 
         hris_user_infos.first_name, 
@@ -28,7 +31,7 @@ const getUserInfoSQL = `
 exports.getUserInfo = async (req, res) => {
     console.log(req.user);
 
-    const user_id = req.user.user_id; 
+    const user_id = req.user.user_id;
 
     try {
         const values = [user_id];
@@ -63,3 +66,48 @@ exports.getAllUserAccounts = async (req, res) => {
         return res.status(500).json({ message: "Error fetching user accounts", error });
     }
 };
+
+exports.createUserAccount = async (req, res) => {
+    const data = req.body;
+    const user_id = uuidv4();
+    const user_info_id = uuidv4();
+    const user_access_permision_id = uuidv4();
+    const user_designation_id = uuidv4();
+
+    const hashedPassword = await bcrypt.hash(data.user_password, 10);
+
+    //TODO: user accounts
+    await pool.execute(
+        `
+        INSERT INTO hris_user_accounts (user_id, user_email, user_password) VALUES (?, ?, ?)
+        `, [user_id, data.user_email, hashedPassword]
+    );
+
+    //TODO: user infos
+    await pool.execute(
+        `
+        INSERT INTO hris_user_infos (user_info_id, user_id, first_name, last_name, personal_email) VALUES (?, ?, ?, ?, ?)
+        `, [user_info_id, user_id, data.first_name, data.last_name, data.user_email]
+    );
+
+    //TODO: hris_user_access_permision
+    data.service_feature_ids.map(async (service_feature_id) => {
+        await pool.execute(
+            `
+            INSERT INTO hris_user_access_permissions (user_access_permision_id, user_id, service_feature_id) VALUES (?, ?, ?)
+            `, [user_access_permision_id, user_id, service_feature_id]
+        );
+    });
+
+    //TODO: user_designations
+    await pool.execute(
+        `
+        INSERT INTO hris_user_designations (user_designation_id, user_id, company_id, job_title_id) VALUES  (?, ?, ?, ?)
+        `, [user_designation_id, user_id, data.company_id, data.job_title_id]
+    );
+
+    return res.status(201).json("user inserted")
+
+
+
+}
